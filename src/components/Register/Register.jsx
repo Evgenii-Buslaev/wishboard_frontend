cc5200a113a6f135461a162001b90e829f3a13e3
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-12-13 16:12:46 +0500

    Refactor: created useRegister

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,23 +1,17 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { register } from "../../redux/action_creators/user";
+import useRegister from "../../hooks/useRegister";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [age, setAge] = useState("");
-  const [sex, setSex] = useState("not selected");
-
-  const dispatch = useDispatch();
-
-  const submit = (e) => {
-    e.preventDefault();
-    if (!name || !password) {
-      alert("Пожалуйста, заполните все обязательные поля");
-    } else {
-      dispatch(register({ name, password, age, sex }));
-    }
-  };
+  const {
+    name,
+    password,
+    age,
+    sex,
+    setName,
+    setPassword,
+    setAge,
+    setSex,
+    submit,
+  } = useRegister();
 
   return (
     <form onSubmit={(e) => submit(e)}>
