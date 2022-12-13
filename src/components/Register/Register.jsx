704e529829e7b5bd4970f9f88f374cad34eb9f28
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-12-13 15:00:08 +0500

    Created register form

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { register } from "../../redux/action_creators/user";
+
 const Register = () => {
-  return <h2>Зарегистрируйтесь в приложении</h2>;
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [age, setAge] = useState("");
+  const [sex, setSex] = useState("not selected");
+
+  const dispatch = useDispatch();
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (!name || !password) {
+      alert("Пожалуйста, заполните все обязательные поля");
+    } else {
+      dispatch(register({ name, password, age, sex }));
+    }
+  };
+
+  return (
+    <form onSubmit={(e) => submit(e)}>
+      <input
+        type="text"
+        placeholder="Имя пользователя"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Пароль"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Возраст"
+        value={age}
+        onChange={(e) => setAge(e.target.value)}
+      />
+      <select
+        placeholder="Пол"
+        value={sex}
+        onChange={(e) => setSex(e.target.value)}
+      >
+        <option value="not selected" disabled>
+          Пол
+        </option>
+        <option value="male">Муж.</option>
+        <option value="female">Жен.</option>
+      </select>
+      <button type="submit">Зарегистрироваться</button>
+    </form>
+  );
 };
 
 export default Register;
