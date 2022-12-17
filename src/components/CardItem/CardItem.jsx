b931feee8a3b7816977be4ae2d9f189d6a9fe323
Evgenii Buslaev <eugenebus144@gmail.com>
2022-12-17 22:55:19 +0500

    Refactor: edited likes schema, added likers list

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -8,6 +8,7 @@ const CardItem = ({ user, data }) => {
 
   const [cardLikes, setCardLikes] = useState(likes.length);
   const [likedByUser, setLikedByUser] = useState(displayUserLike());
+  const [displayLikers, setDisplayLikers] = useState(false);
 
   function displayUserLike() {
     if (!user.loggedIn) {
@@ -27,17 +28,27 @@ const CardItem = ({ user, data }) => {
         dispatch(
           updateCard({
             ...data,
-            likes: data.likes.filter((like) => like !== user.user.name),
+            likes: data.likes.filter((like) => like._id !== user.user._id),
           })
         );
       } else {
         setCardLikes((prevState) => prevState + 1);
         setLikedByUser(true);
         dispatch(
-          updateCard({ ...data, likes: [...data.likes, user.user.name] })
+          updateCard({
+            ...data,
+            likes: [
+              ...data.likes,
+              { _id: user.user._id, name: user.user.name },
+            ],
+          })
         );
       }
     }
+  };
+
+  const toggleDisplayLikers = () => {
+    setDisplayLikers((prevState) => !prevState);
   };
 
   return (
@@ -47,7 +58,21 @@ const CardItem = ({ user, data }) => {
       <p>{text}</p>
       {picture ? <img style={{ width: 300 }} src={picture} alt="card" /> : null}
       <h6>{createdAt}</h6>
-      <h6 onClick={toggleLike}>{cardLikes}</h6>
+      <div onClick={toggleLike}>
+        <h6
+          onMouseEnter={toggleDisplayLikers}
+          onMouseLeave={toggleDisplayLikers}
+        >
+          {cardLikes}
+        </h6>
+        {displayLikers ? (
+          <ul>
+            {data.likes.map((like) => (
+              <li key={like._id}>{like.name}</li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
     </div>
   );
 };
