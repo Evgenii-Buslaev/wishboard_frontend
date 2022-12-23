4f99033a8acc1352e5f3ea756107b826fbb0cb23
Evgenii-Buslaev <eugenebus144@gmail.com>
2022-12-23 17:28:52 +0500

    added content to home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,33 @@
+import { useNavigate } from "react-router-dom";
+
 import styles from "../../scss/components/_home.module.scss";
 
 const Home = () => {
+  const navigator = useNavigate();
   return (
     <div className={styles.container}>
-      <h1>Добро пожаловать!</h1>
+      <div className={styles.begin}>
+        <h1>Добро пожаловать!</h1>
+        <button onClick={() => navigator("/cards")}>Начать работу</button>
+      </div>
+
+      <p className={styles.description}>
+        <h2>Мы представляем сервис для создания поздравительных карточек:</h2>
+        <ul>
+          <li>
+            Напишите любое пожелание для родных и близких, друзей, или даже
+            незнакомых людей;
+          </li>
+          <li>
+            Воспользуйтесь шаблонами поздравлений из созданных карточек, чтобы
+            поздравить человека с важным для него событием;
+          </li>
+          <li>
+            Общайтесь в комментариях, сохраняйте понравившиеся карточки, ставьте
+            лайки.
+          </li>
+        </ul>
+      </p>
     </div>
   );
 };
