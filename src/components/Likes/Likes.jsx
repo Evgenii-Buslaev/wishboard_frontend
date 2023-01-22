d5740acfbd9b0837377bec3cbc2eb5ddecc9b1de
Evgenii Buslaev <eugenebus144@gmail.com>
2023-01-22 13:21:42 +0500

    changed likes list styles

--- src/components/Likes/Likes.jsx
+++ src/components/Likes/Likes.jsx
@@ -4,7 +4,6 @@ const Likes = ({ likes, visible }) => {
   if (!visible) return;
   return (
     <div className={styles.list}>
-      <h5>Лайкнули:</h5>
       <ul>
         {likes.map((liker) => (
           <li key={liker._id}>{liker.name}</li>
