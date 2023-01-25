a49b7d29a37b2903591c2af632f58afa4650d323
Evgenii-Buslaev <eugenebus144@gmail.com>
2023-01-25 16:25:46 +0500

    Validate user like and dislike under comment

--- src/components/CommentItem/CommentItem.jsx
+++ src/components/CommentItem/CommentItem.jsx
@@ -4,21 +4,47 @@ import { ReactComponent as Close } from "../../assets/icons/close.svg";
 import { ReactComponent as Thumb } from "../../assets/icons/thumb.svg";
 
 const CommentItem = ({ comment, remove, update, user }) => {
-  console.log(comment);
-
   const addLike = () => {
-    const newComment = {
-      ...comment,
-      likes: [...comment?.likes, { name: user.name, id: user.userId }],
-    };
+    if (!user) {
+      return;
+    }
+    let newComment;
+    const userLike = comment.likes.find((like) => like.id === user._id);
+    if (userLike) {
+      newComment = {
+        ...comment,
+        likes: [...comment.likes.filter((like) => like.id !== user._id)],
+      };
+    } else {
+      newComment = {
+        ...comment,
+        likes: [...comment.likes, { name: user.name, id: user._id }],
+      };
+    }
     update(newComment);
   };
 
   const addDislike = () => {
-    const newComment = {
-      ...comment,
-      dislikes: [...comment?.dislikes, { name: user.name, id: user.userId }],
-    };
+    if (!user) {
+      return;
+    }
+    let newComment;
+    const userDisike = comment.dislikes.find(
+      (dislike) => dislike.id === user._id
+    );
+    if (userDisike) {
+      newComment = {
+        ...comment,
+        dislikes: [
+          ...comment.dislikes.filter((dislike) => dislike.id !== user._id),
+        ],
+      };
+    } else {
+      newComment = {
+        ...comment,
+        dislikes: [...comment.dislikes, { name: user.name, id: user._id }],
+      };
+    }
     update(newComment);
   };
   return (
@@ -27,11 +53,17 @@ const CommentItem = ({ comment, remove, update, user }) => {
         <h3 className={styles.author}>{comment.author} комментирует:</h3>
         <h4 className={styles.text}>{comment.comment}</h4>
         <div className={styles.reactions}>
-          <Thumb className={styles.thumb} onClick={addLike} />
-          <Thumb
-            className={`${styles.thumb} ${styles.thumb_down}`}
-            onClick={addDislike}
-          />
+          <div className={styles.reaction__cont}>
+            <Thumb className={styles.thumb} onClick={addLike} />
+            <p>{comment.likes.length}</p>
+          </div>
+          <div className={styles.reaction__cont}>
+            <Thumb
+              className={`${styles.thumb} ${styles.thumb_down}`}
+              onClick={addDislike}
+            />
+            <p>{comment.dislikes.length}</p>
+          </div>
         </div>
       </div>
       {comment.author === user?.name ? (
